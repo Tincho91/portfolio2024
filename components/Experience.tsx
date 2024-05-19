c3a7556a39dfc380aa3ae8b487acd0d7b6c73364
Martin Maiaru <tincho_91_15@hotmail.com>
2024-05-20 00:01:13 +0200

    1

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,7 +12,7 @@ const Experience = () => {
       <div className='w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10'>
         {workExperience.map((card) => (
           <Button
-            hey={card.id}
+            key={card.id}
             duration={Math.floor(Math.random() * 10000 + 10000)}
             borderRadius='1.75rem'
             className='flex-1 text-white border-neutral-200 dark:border-slate-800'
